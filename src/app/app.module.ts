c0f016b98d322a9c442270073d0e43b89464e7b6
Yana Yasinvski <jantschick3@gmail.com>
2024-02-13 11:51:25 +0100

    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { StyleDirective } from './directives/style.directive';
-import { IfnotDirective } from './directives/ifnot.directive';
-
+import { AppComponent } from './app.component'
+import { FormsModule } from '@angular/forms'
+import { MultByPipe } from './pipes/mult-by.pipe'
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDirective,
-    IfnotDirective,
+    MultByPipe,
+    ExMarksPipe,
+    FilterPipe,
   ],
   imports: [
     BrowserModule,
