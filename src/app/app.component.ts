44884315948a8af19fa4d3e7837f847276822ec8
Yana Yasinvski <jantschick3@gmail.com>
2024-02-20 14:15:49 +0100

    unit_testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,15 @@
-import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core'
-import { ModalComponent } from './modal/modal.component'
-import { RefDirective } from './ref.directive'
-import { Meta, Title } from '@angular/platform-browser'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-
-  @ViewChild(RefDirective, { static: false }) refDir!: RefDirective
-
-  constructor(
-    private resolver: ComponentFactoryResolver,
-    private title: Title,
-    private meta: Meta
-  ) {
-    this.title.setTitle('App Component Page!')
-    this.meta.addTags([
-      { name: 'keywords', content: 'angular,google,appcomponent' },
-      { name: 'description', content: 'this is app component' }
-    ])
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.')
   }
+  public title = 'angular-unit-tests';
 
-  showModal() {
-    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent)
-    this.refDir.containerRef.clear()
-
-    const component = this.refDir.containerRef.createComponent(modalFactory)
-
-    component.instance.title = 'Dynamic title'
-    component.instance.close.subscribe(() => {
-      this.refDir.containerRef.clear()
-    })
-  }
 }
 
